67d8cbaf16ccfaf7602df3ba1338b6468da25ad4
joprodenhuis <47820822+joprodenhuis@users.noreply.github.com>
2020-05-19 08:50:44 +0200

    Add files via upload

---
+++ closed-captions/sounds.js
@@ -0,0 +1,16 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0, //sound 0//
+	9, //sound 1//
+	12,	//sound 2//
+	15,	//sound 3//
+	16.5,	//sound 4//
+	24.5,	//sound 5//
+	25.5,	//sound 6//
+	26, //sound 7//
+	31,	//sound 8//
+	31.8,	//sound 9//
+	33,	//sound 10//
+	86	//sound 11//
+];
